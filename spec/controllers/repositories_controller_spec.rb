17cbcd07e78ef13c1255b0edb261a319d51742e0
Antonio Jefferson <antoniojeferson96@gmail.com>
2022-06-18 18:22:23 -0300

    test(repositories_controller_spec.rb): tests for the controller

---
+++ spec/controllers/repositories_controller_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe RepositoriesController do
+  let!(:repository) { create(:repository) }
+
+  describe 'GET #index' do
+
+    before { get :index }
+
+    it "render index template" do
+      expect(response).to render_template("repositories/index")
+    end
+
+    it 'returns status 200' do
+      expect(response.status).to eql 200
+    end
+
+    it "assigns @repos" do
+      expect(assigns(:repos)).to eq([repository])
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: repository.id } }
+
+    it "render show template" do
+      expect(response).to render_template("repositories/show")
+    end
+
+    it 'returns status 200' do
+      expect(response.status).to eql 200
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it "redirect to index template" do
+      expect(response).to redirect_to('/')
+    end
+
+    it 'returns status 302' do
+      expect(response.status).to eql 302
+    end
+  end
+
+  describe 'GET #destroy' do
+    before { get :destroy, params: { id: repository.id } }
+
+    it "redirect to index template" do
+      expect(response).to redirect_to('/')
+    end
+
+    it 'returns status 302' do
+      expect(response.status).to eql 302
+    end
+
+    it "assigns @repository" do
+      expect(assigns(:repository)).to eq(repository)
+    end
+
+    it "returns notice" do
+      expect(flash[:notice]).to eq("Repositório do #{repository.name} foi removido!")
+    end
+  end
+
+end
