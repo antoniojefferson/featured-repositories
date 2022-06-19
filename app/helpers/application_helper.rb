c13c51b466888c37455f0da665f67e16406ff063
Antonio Jefferson <antoniojeferson96@gmail.com>
2022-06-19 04:32:02 -0300

    feat(app/helpers/application_helper.rb): adding helper to check language and format date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def date_br(date_us)
+    format_string = english_language ? '%Y-%m-%d' : '%d/%m/%Y'
+    date_us.strftime(format_string)
+  end
+
+  def english_language
+    I18n.locale == :en
+  end
 end
