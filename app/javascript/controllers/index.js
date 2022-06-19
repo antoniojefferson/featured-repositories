c026357a2cc8258243df51571fc40997cfee4b78
Antonio Jefferson <antoniojeferson96@gmail.com>
2022-06-19 04:32:39 -0300

    refactor: removing unused controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,7 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
 import ActionController from "./action_controller"
-application.register("hello", HelloController)
 application.register("action", ActionController)
