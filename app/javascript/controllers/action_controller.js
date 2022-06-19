0e019b6b4f3912a0654c872d668b3757c9574597
Antonio Jefferson <antoniojeferson96@gmail.com>
2022-06-19 04:21:39 -0300

    feat(app/javascript/controllers/action_controller.js): adding methods
    adding method to change language via request and adding logic to send proper prompt message

--- app/javascript/controllers/action_controller.js
+++ app/javascript/controllers/action_controller.js
@@ -1,13 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = { proptMsg: String }
 
   delete(event) {
-    let confirmed = confirm("Deletar repositorio?")
+    let confirmed = confirm(this.proptMsgValue)
 
     if (!confirmed) {
       event.preventDefault()
     }
+  }
+
+  language(event) {
+    window.location.href = `/?locale=${ event.target.checked ? 'en' : 'pt-BR' }`
   }
 
 }
